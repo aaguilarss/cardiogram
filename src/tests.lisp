02ad55356d7dcab614a6f1327f920272ab339f2d
Shapesncats <a.aguilar@ciencias.unam.mx>
2018-10-10 12:50:54 -0500

    test-combination 1

--- src/tests.lisp
+++ src/tests.lisp
@@ -3,156 +3,95 @@
 
 (in-package :cl-user)
 (defpackage :cardio-tests
-  (:use :cl))
+  (:use :cl :lol))
 (in-package :cardio-tests)
 
-;;; Test class
 
-(opts
-  :before this ; Run the test before this
-  :after that ; Run the test after that
-  :arround this ; Run the test before and after this
-  :when (and this that) ; Run the test only if this condition is met
-  :unless (and this that) ; Run the test unless the condition is met
-  :time-limit 0.5 ; Run only for 0.5 seconds
-  :fixtures (v1 v2 v3) ; Fix these three variables
-  :on-exec nil) ; Run on execute
+(defvar +tests+ (make-hash-table))
 
+
+;;; test class
 
 (defclass test ()
-  ((name :initarg :name :accessor)
-   (forms
+  ((forms
      :initarg :forms
-     :accessor test-forms
-     :type function) ; The actual test forms
+     :accessor forms)
    (before
      :initarg :before
-     :accessor)
-   (after :initarg :after :accessor)
-   (arround :initarg :arround :accessor)
-   (when :initarg :when :accessor) ; Default is t
-   (unless :initarg :unless :accessor) ; Default is nil
-   (time-limit :initarg :time-limit)
-   (fixtures :initarg :fixtures)
-   (on-exec :initarg :on-exec)))
+     :accessor before
+     :initform nil)
+   (after
+     :initarg :after
+     :accessor after
+     :initform nil)
+   (around
+     :initarg :around
+     :accessor around
+     :initform nil)
+   (when
+     :initarg :when
+     :accessor test-when
+     :initform t)
+   (time-limit
+     :initarg :time-limit
+     :accessor time-limit
+     :initform nil)
+   (fixes
+     :initarg :fixes
+     :accessor fixes
+     :initform nil)))
 
 
-(defmethod testcall ((test test))
-  "Call test test"
-  (dolist (e (before test)) (eval-test e)) ; Eval before tests
-  (dolist (e (arround test)) (eval-test e)) ; Eval arround tests
-  (when (and (twhen test)
-             (not (tunless test)))
-    (funcall (test-forms test)))
-  (dolist (e (arround test)) (eval-test e)) ; Eval arround tests
-  (dolist (e (after test)) (eval-test e))) ; Eval after tests
+(defmacro deftest (name options &body body)
+  `(setf (gethash ',name +tests+)
+         (make-instance 'test
+           :forms (lambda () ,@(alexandria:parse-body body :documentation t))
+           :when ,(getf options :when)
+           :before ,(getf options :before)
+           :after ,(getf options :after)
+           :around ,(getf options :around)
+           :time-limit ,(getf options :time-limit)))
+  `(alexandria:maphash-values
+     (lambda (x)
+       (loop for i in (reverse ,(getf options :around))
+             do (setf (before x) (adjoin i (before x))))) +tests+)
+  `(alexandria:maphash-values
+     (lambda (x)
+       (loop for i in (reverse ,(getf options :before))
+             do (setf (before x) (adjoin i (before x))))) +tests+)
+  `(alexandria:maphash-values
+     (lambda (x)
+       (loop for i in (reverse ,(getf options :after))
+             do (setf (before x) (adjoin i (before x))))) +tests+)
+  `(setf (symbol-function ',name)
+         (dlambda
+           (:run () "Running")
+           (:info () "Info"))))
 
 
 
+(defgenetic eval-test (test)
+  (:documentation "Evaluate a test"))
 
-(lambda ()
-  form
-  :skip
-  form)
+(defmethod eval-test ((obj test))
+  (funcall (forms test)))
 
+(defmethod eval-test :around ((obj test))
+  (dolist (tst (around obj)) (eval-test tst)))
 
+(defmethod eval-test :before ((obj test))
+  (dolist (tst (before obj)) (eval-test tst)))
 
-(let ((skip '(a)))
-  form
-  form
-  :a form
-  form)
-
-(tagbody
-  :tag
-  :tag
-  :tag
-  :tag
-  :a
-  :tag)
+(defmethod eval-test :after ((obj test))
+  (dolist (tst (after obj))) (eval-test tst))
 
 
-(only)
+; (opts
+;   :before this ; Run the test before this
+;   :after that ; Run the test after that
+;   :arround this ; Run the test before and after this
+;   :when (and this that) ; Run the test only if this condition is met
+;   :time-limit 0.5 ; Run only for 0.5 seconds
+;   :fix (v1 v2 v3) ; Fix these three variables
+;   :on-exec nil) ; Run on execute
 
-(tagbody
-  :1
-  :2
-  :3
-  :a
-  :5
-  :6)
-
-(defun pop)
-
-
-(let ((skip '(2)))
-  (tagbody
-    :eval
-    (if skip
-      (go 1)
-      (go :end))
-    1 (print "hello")
-    (go :eval)
-    2 (print " bye")
-    (go :eval)
-    3 (print " friend")
-    (go :eval)
-    :end))
-
-
-(defmacro sklambda (name &body body)
-  "lambda form that skips when it's name is passed as an argument"
-  (alexandria:with-gensyms (x)
-    `(lambda (&optional ,x)
-       (unless (if (listp ,x)
-                 (member ,name ,x)
-                 (eql ,name ,x))
-         ,@body))))
-
-
-(it this that :a)
-
-(lambda (x)
-  (unless (if (listp x)
-            (member :name x)
-            (eql :name x))
-    (do this)))
-
-(lambda (x)
-  (unless (eql x :skip)
-    forms))
-
-(sklambda :skip
-          (print "hello"))
-
-(if (x)
-  (nil)
-  (block nil
-         forms))
-
-
-(tagbody
-  form)
-
-(prog ((a 3))
-      1 (princ "hello") (go a)
-      2 (princ "hey")
-      3 (princ ", lisa"))
-
-
-:skip
-(is :skip)
-
-(defmethod eval-test ((test test) &key skip)
-  (tagbody
-    :before
-    :after
-    :arround
-    :test
-    ()))
-
-(defmethod eval-test ((test test) &key skip))
-
-(tagbody
-  :
-  :assertion)
