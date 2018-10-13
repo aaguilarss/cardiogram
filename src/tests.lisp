ad7a993a36556e07230636cc483d61679598052f
Shapesncats <a.aguilar@ciencias.unam.mx>
2018-10-13 04:50:39 -0500

    Test combinations working a bit. Fixtures working

--- src/tests.lisp
+++ src/tests.lisp
@@ -2,9 +2,11 @@
 ;;; (c) 2018 Abraham Aguilar <a.aguilar@ciencias.unam.mx>
 
 (in-package :cl-user)
-(defpackage :cardio-tests
+(defpackage :cardio/tests
   (:use :cl :lol))
-(in-package :cardio-tests)
+(in-package :cardio/tests)
+(annot:enable-annot-syntax)
+
 
 (defvar +tests+ (make-hash-table))
 
@@ -27,28 +29,22 @@
      :initarg :around
      :accessor around
      :initform nil)
-   (when
-     :initarg :when
-     :accessor test-when
-     :initform t)
    (time-limit
      :initarg :time-limit
      :accessor time-limit
-     :initform nil)
-   (fixes
-     :initarg :fixes
-     :accessor fixes
      :initform nil)))
 
-
+@export
 (defmacro deftest (name options &body body)
   "Creates an instance of test and saves it to +TESTS+.
   Then defines a dispatche function of name NAME"
   `(progn
      (setf (gethash ',name +tests+)
            (make-instance 'test
-             :forms (lambda () ,@(alexandria:parse-body body :documentation t))
-             :when ,(getf options :when)
+             :forms (lambda () (with-fixtures ,(getf options :fix)
+                                              (when ,(or (getf options :when) t)
+                                                (f!block nil
+                                                  ,@(alexandria:parse-body body :documentation t)))))
              :time-limit ,(getf options :time-limit)))
      (maphash (lambda (k v)
                 (when (member k ',(getf options :around))
@@ -64,23 +60,26 @@
                         (adjoin ',name (after v))))) +tests+)
     (setf (symbol-function ',name)
           (lol:dlambda
-            (:run () (eval-test (gethash ',name +tests+)))
+            (:run (&optional &key skip) (eval-test ',name skip))
             (:info () "Info")))
     ',name))
 
 
-
-
-(defun eval-test ((test test))
-  (dolist (a (around test))
-    (eval-test (gethash a +tests+)))
-  (dolist (b (before test))
-    (eval-test (gethash b +tests+)))
-  (funcall (forms test))
-  (dolist (a (after test))
-    (eval-test (gethash a +tests+)))
-  (dolist (a (around test))
-    (eval-test (gethash a +tests+))))
+(defun eval-test (test skip)
+  (let ((tst (gethash test +tests+)))
+    (dolist (a (around tst))
+      (unless (if (listp skip) (member a skip) (eql a skip))
+        (eval-test a skip)))
+    (dolist (a (before tst))
+      (unless (if (listp skip) (member a skip) (eql a skip))
+        (eval-test a skip)))
+    (funcall (forms tst))
+    (dolist (a (after tst))
+      (unless (if (listp skip) (member a skip) (eql a skip))
+        (eval-test a skip)))
+    (dolist (a (reverse (around tst)))
+      (unless (if (listp skip) (member a skip) (eql a skip))
+        (eval-test a skip)))))
 
 ; (opts
 ;   :before this ; Run the test before this
