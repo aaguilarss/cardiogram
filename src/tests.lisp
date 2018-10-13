7e8f0f364d0ec48d8a5ef1eaf85326f92b2ec330
Shapesncats <a.aguilar@ciencias.unam.mx>
2018-10-13 00:34:30 -0500

    Test combination v1

--- src/tests.lisp
+++ src/tests.lisp
@@ -6,7 +6,6 @@
   (:use :cl :lol))
 (in-package :cardio-tests)
 
-
 (defvar +tests+ (make-hash-table))
 
 
@@ -43,48 +42,45 @@
 
 
 (defmacro deftest (name options &body body)
-  `(setf (gethash ',name +tests+)
-         (make-instance 'test
-           :forms (lambda () ,@(alexandria:parse-body body :documentation t))
-           :when ,(getf options :when)
-           :before ,(getf options :before)
-           :after ,(getf options :after)
-           :around ,(getf options :around)
-           :time-limit ,(getf options :time-limit)))
-  `(alexandria:maphash-values
-     (lambda (x)
-       (loop for i in (reverse ,(getf options :around))
-             do (setf (before x) (adjoin i (before x))))) +tests+)
-  `(alexandria:maphash-values
-     (lambda (x)
-       (loop for i in (reverse ,(getf options :before))
-             do (setf (before x) (adjoin i (before x))))) +tests+)
-  `(alexandria:maphash-values
-     (lambda (x)
-       (loop for i in (reverse ,(getf options :after))
-             do (setf (before x) (adjoin i (before x))))) +tests+)
-  `(setf (symbol-function ',name)
-         (dlambda
-           (:run () "Running")
-           (:info () "Info"))))
+  "Creates an instance of test and saves it to +TESTS+.
+  Then defines a dispatche function of name NAME"
+  `(progn
+     (setf (gethash ',name +tests+)
+           (make-instance 'test
+             :forms (lambda () ,@(alexandria:parse-body body :documentation t))
+             :when ,(getf options :when)
+             :time-limit ,(getf options :time-limit)))
+     (maphash (lambda (k v)
+                (when (member k ',(getf options :around))
+                  (setf (around v)
+                        (adjoin ',name (around v))))) +tests+)
+     (maphash (lambda (k v)
+                (when (member k ',(getf options :before))
+                  (setf (before v)
+                        (adjoin ',name (before v))))) +tests+)
+     (maphash (lambda (k v)
+                (when (member k ',(getf options :after))
+                  (setf (after v)
+                        (adjoin ',name (after v))))) +tests+)
+    (setf (symbol-function ',name)
+          (lol:dlambda
+            (:run () (eval-test (gethash ',name +tests+)))
+            (:info () "Info")))
+    ',name))
 
 
 
-(defgenetic eval-test (test)
-  (:documentation "Evaluate a test"))
 
-(defmethod eval-test ((obj test))
-  (funcall (forms test)))
-
-(defmethod eval-test :around ((obj test))
-  (dolist (tst (around obj)) (eval-test tst)))
-
-(defmethod eval-test :before ((obj test))
-  (dolist (tst (before obj)) (eval-test tst)))
-
-(defmethod eval-test :after ((obj test))
-  (dolist (tst (after obj))) (eval-test tst))
-
+(defun eval-test ((test test))
+  (dolist (a (around test))
+    (eval-test (gethash a +tests+)))
+  (dolist (b (before test))
+    (eval-test (gethash b +tests+)))
+  (funcall (forms test))
+  (dolist (a (after test))
+    (eval-test (gethash a +tests+)))
+  (dolist (a (around test))
+    (eval-test (gethash a +tests+))))
 
 ; (opts
 ;   :before this ; Run the test before this
