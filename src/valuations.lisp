fa1eb18d20b961b3d7635c70c81c1e9882741410
Shapesncats <a.aguilar@ciencias.unam.mx>
2018-10-18 00:59:14 -0500

    quick commit

--- src/valuations.lisp
+++ src/valuations.lisp
@@ -3,7 +3,8 @@
 
 (in-package :cl-user)
 (defpackage :cardio/valuations
-  (:use :cl))
+  (:use :cl
+        :cardio/toolkit))
 (in-package :cardio/valuations)
 (annot:enable-annot-syntax)
 
@@ -67,10 +68,10 @@
 (defmacro defval (name args &body body)
   `(progn
      (setf (val-definition ',name)
-           (lambda ,args
+           (flambda ,args
              ,@(alexandria:parse-body body :documentation t)))
      (defun ,name ,args
-       (call-valuation +format+ ',name (list ,@args)))))
+       (call-valuation +format+ ',name (list ,@(mapcar (lambda (x) `(delay ,x)) args))))))
 
 
 
