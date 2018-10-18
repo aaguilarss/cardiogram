fca9670e375a1a98cb38baddfa58fa55ae53e7d2
Shapesncats <a.aguilar@ciencias.unam.mx>
2018-10-17 20:01:09 -0500

    Valuations working, Simple and Binary formats added

--- src/valuations.lisp
+++ src/valuations.lisp
@@ -8,7 +8,8 @@
 (annot:enable-annot-syntax)
 
 
-; Ultra basic functionality
+; Basic functionality
+;;; Formats
 
 (defparameter +formats+ (make-hash-table))
 (defparameter +format+ nil)
@@ -26,12 +27,17 @@
           ,@(alexandria:parse-body body :documentation t))))
 
 (defmacro deformat! (name args &body body)
-  "Define a format. This macro injects two variables: TEST and STREAM"
-  `(setf (find-format name)
+  "Define a format. This macro injects two variables: TEST and STREAM as
+  the first and second required arguments of the formatter funcion it
+  defines."
+  `(setf (find-format ',name)
          (lambda ,(append '(test stream) args)
            ,@(alexandria:parse-body body :documentation t))))
 
-(deformat! simple (expected form valuation-name)
+
+;; Built-in formats
+
+(deformat! simple (form expected valuation-name)
   "The simple format. Used as DEFAULT"
   (format stream "~a - ~:[FAILED ~a was expected. Got ~a~%~;PASS~%~]"
           valuation-name test form expected))
@@ -41,106 +47,84 @@
   (format stream "~:[0~;1~]~%" test))
 
 
-(lambda (expected form &optional &key s)
-  (funcall (find-format 'simple)
-           (eql expected form) t expected form (valuation-name "S")))
-
 
 ;;; Valuations
 
 (defparameter +valuations+ (make-hash-table))
 
 
-(defun symbol-valuation (symbol)
+(defun val-definition (symbol)
   (gethash symbol +valuations+))
 
-(defun (setf symbol-valuation) (new symbol)
+(defun (setf val-definition) (new symbol)
   (setf (gethash symbol +valuations+) new))
 
-(defmacro defvaluation (name args body)
-  `(setf (symbol-valuation ,name)
-         (lambda ,args
-           ,@body)))
-
-
-(defvaluation is (expected form)
-  (eql expected form))
-
-
 (defgeneric call-valuation (format valuation args &optional stream)
-  (:documentation "Call VALUAION with FORMAT and ARGS.")
+  (:documentation "Defines how of FORMAT handles the result of VALUATION")
   (:argument-precedence-order format valuation args))
 
 
-
-(call-valuation (make-instance 'is :expected this :form that)
-                +format+)
-
-(call-valuation (make-instance 'is :expected this :form that)
-                +format+
-                '(this that))
-
-(defmethod call-valuation ((valuation cardio-valuation) (format binary) args)
-  (let ((result (apply (valuator valuation) args)))
-    (funcall (formatter format) result)))
+(defmacro defval (name args &body body)
+  `(progn
+     (setf (val-definition ',name)
+           (lambda ,args
+             ,@(alexandria:parse-body body :documentation t)))
+     (defun ,name ,args
+       (call-valuation +format+ ',name (list ,@args)))))
 
 
 
+;; Built-in Valuations
 
+(defval true (form)
+  (when form t))
 
-(defval is (expected form))
+(defval false (form)
+  (unless form t))
 
+(defval pass (form)
+  (declare (ignore form))
+  t)
 
-(defmacro x? (&optional (x nil supplied-p))
-  (if supplied-p
-    x
-    (print ":(")))
+(defval fail (form)
+  (declare (ignore form))
+  nil)
 
-(defmacro tr (&optional (x nil x-p))
-  (unless x-p
-    (print ":("))
-  x)
+(defval is (form expected)
+  (eql expected form))
 
+(defval isnt (form expected)
+  (not (eql expected form)))
 
+(defval is-values (form expected)
+  (equal (multiple-value-list form)
+         (multiple-value-list expected)))
 
+(defval isnt-values (form expected)
+  (not
+    (equal (multiple-value-list form)
+           (multiple-value-list expected))))
 
-; Example of a is-not valuation
+(defval eql-types (form expected)
+  (eql (type-of form)
+       (type-of expected)))
 
-(lambda (expected form)
-  (format t "IS-NOT - ~:[FAILED ~a was expected not to be ~a~%~;PASSED~%~]"
-          (not (eql expected form))
-          form
-          expected))
-
-(lambda (expected form)
-  (format t "IS - ~:[FAILED ~a was expected to be ~a~%~;PASSED~%~]"
-          (eql expected form)
-          form
-          expected))
-
-(lambda (form)
-  (format t "IS - ~:[FAILED ~a was expected to be ~a~%~;PASSED~%~]"
-          (eql form t)
-          form
-          t))
-
-(lambda (form)
-  (format t "IS - ~:[FAILED ~a was expected to be ~a~%~;PASSED~%~]"
-          (eql form nil)
-          form
-          nil))
+(defval expands-1 (form expected)
+  (equal (macroexpand-1 form)
+         expected))
 
 
 
 
-(defmacro defval (name args msg test)
-  `(defun ,name ,args
-    (format t "~w - ~:[FAILED ~a was  not to be ~a~%~;PASSED~%~]"
-            ',name ,test form expected)))
+;;; Format implementation
 
-(lambda defval)
+(defmethod call-valuation ((format (eql 'binary)) val args &optional (stream t))
+  (funcall (find-format format) (apply (val-definition val) args) stream))
 
-
-
-(defval is (expected form)
-  (eql))
+(defmethod call-valuation ((format (eql 'simple)) val args &optional (stream t))
+  (funcall (find-format format)
+           (apply (val-definition val) args)
+           stream
+           (car args)
+           (cadr args)
+           val))
